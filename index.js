b419e8d603e2f98471cfb6ed4b32514a42b10637
YASH-GU24 <yashg.it.20@nitj.ac.in>
2022-04-26 17:30:38 +0530

    added files

---
+++ index.js
@@ -0,0 +1,42 @@
+const express =  require('express')
+const app = express()
+const path = require('path')
+const weaviate = require("weaviate-client");
+const bodyParser = require('body-parser');
+const {PythonShell} = require('python-shell')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'views')));
+app.engine('html', require('ejs').renderFile);
+let initial_path = path.join(__dirname, "views");
+const client = weaviate.client({
+    scheme: 'http',
+    host: 'localhost:8080',
+    headers: {authorization: 'Bearer <put your token here>'}
+  });
+app.get('/', (req, res) => {
+    res.render(path.join(initial_path, "login_page.ejs"),{message:""});
+})
+app.post('/login', (req, res) => {
+    let username = req.body['username'];
+    let password = req.body['password'];
+    let url = req.body['cluster_url']
+    let options = {
+        args: [username,password,url]
+    }
+    console.log(options)
+    PythonShell.run('python_scripts/script.py', options, function (err, results) {
+        if (err) {
+            res.render(path.join(initial_path, "login_page.ejs"),{message:"Invalid URL , Please enter a valid URL"});
+        };
+        console.log('results: %j', results);
+        if(results[1]=="Invalid creds")
+        {
+            res.render(path.join(initial_path, "login_page.ejs"),{message:"Invalid username or password"});
+        }
+        if(results[1]=="Client is Ready!")
+        {
+            res.render("output.html");
+        }
+      });
+})
+app.listen(process.env.PORT || 3000)
---
+++ python_scripts/script.py
@@ -0,0 +1,77 @@
+import sys
+from getpass import getpass # hide password
+import weaviate # to communicate to the Weaviate instance
+from weaviate.wcs import WCS
+import time
+import pandas as pd
+import pandas_profiling as pp
+
+def get_columns():
+  obj =client.schema.get()
+  column_names=[]
+  for i in range (0,len(obj['classes'])):
+    for j in range (0,len(obj['classes'][i]['properties'])):
+      column_names.append(obj['classes'][i]['properties'][j]['name'])
+
+  return column_names 
+
+def get_size(class_name):
+  result = client.query.aggregate(class_name) \
+    .with_fields('meta { count }') \
+    .do()
+  return result['data']['Aggregate'][class_name][0]['meta']['count']
+
+def get_classname(i):
+  obj =client.schema.get()
+  return obj['classes'][i]['class']
+
+def generate_profile():
+  column_names=get_columns()
+  class_name=get_classname(0)
+  data_size=get_size(class_name)
+  result = client.query.get(class_name=class_name, properties=column_names)\
+      .with_limit(data_size)\
+      .do()
+  data={}
+  for i in range(0,len(column_names)):
+    column_data=[]
+    for j in range (0,data_size):
+      try:
+        column_data.append(result['data']['Get'][class_name][j][column_names[i]])
+      except:
+        print(i,j)
+    name=column_names[i]
+    d={name:column_data}
+    data.update(d)
+  df = pd.DataFrame(data)
+  profile = pp.ProfileReport(df)
+  profile.to_file("/home/yash/Documents/weaviate-data-profiling/views/output.html")
+
+
+my_credentials = weaviate.auth.AuthClientPassword(username=sys.argv[1], password=sys.argv[2])
+# my_credentials = weaviate.auth.AuthClientPassword(username=input(), password=input())
+print(my_credentials)
+try:
+  my_wcs = WCS(my_credentials)
+except:
+    print("Invalid creds")
+    exit()
+
+
+cluster_url=sys.argv[3]
+# cluster_url=input()
+try:
+  client = weaviate.Client(cluster_url,my_credentials)
+except:
+    print("INVALID URL!!!!!!!!")
+    exit()
+# while(client.is_ready()!= True):
+#     time.sleep(1)
+
+if(client.is_ready()== True):
+    print("Client is Ready!")
+    generate_profile()
+else:
+    print("INVALID URL!!!!!!!!")
+
+
